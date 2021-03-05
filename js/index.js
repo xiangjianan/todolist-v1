d18108f8a87e97ca1aefef955adeb16f1c7f17cb
xjn <xjn@xjn.local>
2021-03-06 05:04:00 +0800

    V2.2.0 控制台更新

--- js/index.js
+++ js/index.js
@@ -272,7 +272,11 @@ $(function () {
     });
 
     console.log(`
-博客园：https://www.cnblogs.com/xiangjianan/
+Email :\tXJN9872@163.com
+
+WeChat:\tinMyLife2021
+
+Page  :\twww.helloxjn.com
 
 `);
 });
